8ce2e549bd795589ab74b80cb32014a27c4f0536
OilyPotato819 <o.schneider@share.epsb.ca>
2022-05-28 00:57:38 -0600

    doing things

--- index.js
+++ index.js
@@ -20,12 +20,41 @@ function downloadImage(url, filepath) {
    });
 }
 
+async function getAvatar(userId) {
+   return new Promise((resolve) => {
+      client.guilds.cache
+         .get('964752875832111104')
+         .members.fetch(userId)
+         .then((user) => {
+            downloadImage(
+               user.displayAvatarURL(),
+               'D:\\Coding\\GitHub_Repos\\Clone-Bots\\profile_pics'
+            ).then(({ filename }) => {
+               resolve(filename);
+            });
+         });
+   });
+}
+
 client.once('ready', () => {
    console.log(`Logged in as ${client.user.tag}`);
+
+   (async function () {
+      const filePath = await getAvatar('270904126974590976');
+
+      client.user.setAvatar(filePath).then(function () {
+         fs.unlink(filePath, (err) => {
+            if (err) {
+               console.error(err);
+               return;
+            }
+         });
+      });
+   })();
 });
 
 client.on('messageCreate', (message) => {
    if (message.author.bot) return;
 
-   downloadImage(message.member.displayAvatarURL(), 'D:\\Coding\\GitHub Repos\\Clone-Bots');
+   console.log(message.guild.members.cache.get('799416383611863071'));
 });
