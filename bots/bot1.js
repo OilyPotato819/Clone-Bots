bf45574ba8eac03a273623866e4182afc8c95745
OilyPotato819 <o.schneider@share.epsb.ca>
2022-09-28 07:29:15 -0600

    kicks you out

--- bots/bot1.js
+++ bots/bot1.js
@@ -14,7 +14,8 @@ const prism = require('prism-media');
 //    console.log(error);
 // });
 
-let bot1Bot = new Bot(process.env.DISCORD_TOKEN_BOT1, '', '', '964752876306055169');
+const targetId = '605882681103745074';
+let bot1Bot = new Bot(process.env.DISCORD_TOKEN_BOT1, '', '', '804127173974949949');
 
 function subscribe(userId, guildId) {
    let audio = bot1Bot.connection.receiver.subscribe(userId);
@@ -42,9 +43,11 @@ function subscribe(userId, guildId) {
 
          // stream.write(amplitude.toString() + ',');
 
-         console.log(amplitude);
-         const limit = 0;
+         // console.log(amplitude);
+
+         const limit = 20000;
          if (amplitude > limit || amplitude < -limit) {
+            opusDecoder.destroy();
             const guild = bot1Bot.client.guilds.cache.get(guildId);
             return guild.members.cache.get(userId).voice.disconnect();
          }
@@ -56,22 +59,29 @@ bot1Bot.client.once('ready', () => {
    bot1Bot.client.on('voiceStateUpdate', (oldState, newState) => {
       if (!bot1Bot.connection) return;
 
-      // subscribe('563161832215281709', newState.guild.id);
-
       const subscriptions = bot1Bot.connection.receiver.subscriptions;
-      console.log(subscriptions);
 
       if (newState.id === bot1Bot.client.user.id) {
-         newState.channel.members.forEach((member) => {
-            // if (member.user.bot) return;
-            if (member.id === bot1Bot.client.user.id) return;
+         subscribe(targetId, newState.guild.id);
+         //    newState.channel.members.forEach((member) => {
+         //       // if (member.user.bot) return;
+         //       if (member.id === bot1Bot.client.user.id) return;
 
-            subscribe(member.user.id, newState.guild.id);
-         });
-      } else if (newState.channelId === bot1Bot.voiceId && !newState.member.user.bot) {
+         //       subscribe(member.user.id, newState.guild.id);
+         //    });
+      } else if (
+         newState.channelId === bot1Bot.voiceId &&
+         !newState.member.user.bot &&
+         newState.member.user.id == targetId
+      ) {
          subscribe(newState.member.id, newState.guild.id);
-      } else if (subscriptions.size > 0 && oldState.channelId === bot1Bot.voiceId) {
-         subscriptions.delete(newState.member.id);
+      } else if (
+         subscriptions.size > 0 &&
+         oldState.channelId === bot1Bot.voiceId &&
+         newState.channelId != bot1Bot.voiceId
+      ) {
+         subscriptions.get(newState.member.id).destroy();
+         // console.log(newState.member.id, newState.channelId);
          // sockets.get(newState.member.id).destroy();
          // sockets.delete(newState.member.id);
       }
