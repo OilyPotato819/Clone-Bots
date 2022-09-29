4ab78b2bfa8000c4650de71d8ea8e9eca8ebbeaf
OilyPotato819 <o.schneider@share.epsb.ca>
2022-09-28 19:54:06 -0600

    moved to brawler

--- bots/bot1.js
+++ bots/bot1.js
@@ -14,45 +14,11 @@ const prism = require('prism-media');
 //    console.log(error);
 // });
 
-const targetId = '605882681103745074';
-let bot1Bot = new Bot(process.env.DISCORD_TOKEN_BOT1, '', '', '804127173974949949');
+const targetId = '563161832215281709';
+let bot1Bot = new Bot(process.env.DISCORD_TOKEN_BOT1, '', '', '964752876306055169');
 
 function subscribe(userId, guildId) {
    let audio = bot1Bot.connection.receiver.subscribe(userId);
-
-   const opusDecoder = new prism.opus.Decoder({
-      frameSize: 960,
-      channels: 2,
-      rate: 48000,
-   });
-
-   audio.pipe(opusDecoder).pipe(fs.createWriteStream('graph/audio-output.bin'));
-
-   const stream = fs.createWriteStream('graph/output.csv');
-
-   opusDecoder.on('data', (chunk) => {
-      const wordAmount = Buffer.byteLength(chunk) / 2;
-
-      let chunkIndex = 0;
-      for (let word = 0; word < wordAmount; word++, chunkIndex += 2) {
-         const hex0 = chunk[0 + chunkIndex].toString(16).padStart(2, '0');
-         const hex1 = chunk[1 + chunkIndex].toString(16).padStart(2, '0');
-
-         const hex = `0x${hex1}${hex0}`;
-         const amplitude = +hex > 0x7fff ? +hex - 0x10000 : +hex;
-
-         // stream.write(amplitude.toString() + ',');
-
-         // console.log(amplitude);
-
-         const limit = 20000;
-         if (amplitude > limit || amplitude < -limit) {
-            opusDecoder.destroy();
-            const guild = bot1Bot.client.guilds.cache.get(guildId);
-            return guild.members.cache.get(userId).voice.disconnect();
-         }
-      }
-   });
 }
 
 bot1Bot.client.once('ready', () => {
