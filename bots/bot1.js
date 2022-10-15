0c99cd8258f6ec844244a1f7f150c009636b9365
OilyPotato819 <o.schneider@share.epsb.ca>
2022-10-15 11:45:26 -0600

    qsfdsdf

--- bots/bot1.js
+++ bots/bot1.js
@@ -3,6 +3,25 @@ require('dotenv').config({ path: '../.env' });
 const fs = require('fs');
 const { createAudioResource, createAudioPlayer } = require('@discordjs/voice');
 const prism = require('prism-media');
+const express = require('express');
+const app = express();
+let data = {};
+
+app.get('/', (req, res) => {
+   res.sendFile('D:/Coding/GitHub_Repos/Clone-Bots/graph/graph.html');
+});
+
+app.get('/graph.js', function (req, res) {
+   res.sendFile('D:/Coding/GitHub_Repos/Clone-Bots/graph/graph.js');
+});
+
+app.get('/getData', (req, res) => {
+   res.send(data);
+});
+
+app.listen(3000, () => {
+   console.log('App listening on port 3000');
+});
 
 // let fillerSockets = [];
 // let sockets = new Map();
@@ -14,42 +33,42 @@ const prism = require('prism-media');
 //    console.log(error);
 // });
 
-const targetId = '563161832215281709';
-let bot1Bot = new Bot(process.env.DISCORD_TOKEN_BOT1, '', '', '964752876306055169');
+// const targetId = '563161832215281709';
+// let bot1Bot = new Bot(process.env.DISCORD_TOKEN_BOT1, '', '', '964752876306055169');
 
-function subscribe(userId, guildId) {
-   let audio = bot1Bot.connection.receiver.subscribe(userId);
-}
+// function subscribe(userId, guildId) {
+//    let audio = bot1Bot.connection.receiver.subscribe(userId);
+// }
 
-bot1Bot.client.once('ready', () => {
-   bot1Bot.client.on('voiceStateUpdate', (oldState, newState) => {
-      if (!bot1Bot.connection) return;
+// bot1Bot.client.once('ready', () => {
+//    bot1Bot.client.on('voiceStateUpdate', (oldState, newState) => {
+//       if (!bot1Bot.connection) return;
 
-      const subscriptions = bot1Bot.connection.receiver.subscriptions;
+//       const subscriptions = bot1Bot.connection.receiver.subscriptions;
 
-      if (newState.id === bot1Bot.client.user.id) {
-         subscribe(targetId, newState.guild.id);
-         //    newState.channel.members.forEach((member) => {
-         //       // if (member.user.bot) return;
-         //       if (member.id === bot1Bot.client.user.id) return;
+//       if (newState.id === bot1Bot.client.user.id) {
+//          subscribe(targetId, newState.guild.id);
+//          //    newState.channel.members.forEach((member) => {
+//          //       // if (member.user.bot) return;
+//          //       if (member.id === bot1Bot.client.user.id) return;
 
-         //       subscribe(member.user.id, newState.guild.id);
-         //    });
-      } else if (
-         newState.channelId === bot1Bot.voiceId &&
-         !newState.member.user.bot &&
-         newState.member.user.id == targetId
-      ) {
-         subscribe(newState.member.id, newState.guild.id);
-      } else if (
-         subscriptions.size > 0 &&
-         oldState.channelId === bot1Bot.voiceId &&
-         newState.channelId != bot1Bot.voiceId
-      ) {
-         subscriptions.get(newState.member.id).destroy();
-         // console.log(newState.member.id, newState.channelId);
-         // sockets.get(newState.member.id).destroy();
-         // sockets.delete(newState.member.id);
-      }
-   });
-});
+//          //       subscribe(member.user.id, newState.guild.id);
+//          //    });
+//       } else if (
+//          newState.channelId === bot1Bot.voiceId &&
+//          !newState.member.user.bot &&
+//          newState.member.user.id == targetId
+//       ) {
+//          subscribe(newState.member.id, newState.guild.id);
+//       } else if (
+//          subscriptions.size > 0 &&
+//          oldState.channelId === bot1Bot.voiceId &&
+//          newState.channelId != bot1Bot.voiceId
+//       ) {
+//          subscriptions.get(newState.member.id).destroy();
+//          // console.log(newState.member.id, newState.channelId);
+//          // sockets.get(newState.member.id).destroy();
+//          // sockets.delete(newState.member.id);
+//       }
+//    });
+// });
--- graph/graph.js
+++ graph/graph.js
@@ -1,28 +1,14 @@
 let cnv = document.getElementById('canvas');
 let ctx = cnv.getContext('2d');
 
-cnv.height = 400;
+fetch('http://localhost:3000/getData')
+   .then((response) => response.json())
+   .then((data) => {
+      cnv.height = 400;
 
-const zoomX = 0.005;
-const zoomY = 0.006;
-const middle = 200;
+      const zoomX = 0.005;
+      const zoomY = 0.006;
+      const middle = 200;
 
-let chartValues;
-let xmlhttp = new XMLHttpRequest();
-
-xmlhttp.open('GET', 'output.csv', false);
-xmlhttp.send();
-chartValues = xmlhttp.responseText.split(',');
-
-cnv.width = chartValues.length * zoomX;
-
-let x = 0;
-
-ctx.beginPath();
-ctx.moveTo(0, middle);
-
-chartValues.forEach((element, index) => {
-   ctx.lineTo(x * zoomX, middle - +element * zoomY);
-   x++;
-});
-ctx.stroke();
+      console.log(data.frequencies);
+   });
